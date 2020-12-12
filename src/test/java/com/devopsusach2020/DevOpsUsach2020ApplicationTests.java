da1c14fac9f0de44f33f39208ffa7de83eb48430
sebassabes <sebastian.andres.aracena@gmail.com>
2020-12-12 10:24:58 -0300

    ejemplo con gradle

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsusach2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsUsach2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+        @Test 
+        void  test_nuevo(){
+        }
+
+}
